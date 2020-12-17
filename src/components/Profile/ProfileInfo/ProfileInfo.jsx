b41b55f68094ac5a86c8aa3a61b657abcfbe4164
Artem <artbreenche@gmail.com>
2020-12-17 21:02:13 +0200

    profile page, ajax, api

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import s from './ProfileInfo.module.css';
+import Preloader from "../../common/Preloader/Preloader";
 
-
-const ProfileInfo = () => {
+const ProfileInfo = (props) => {
+	 if (!props.profile) {
+        return <Preloader />
+    }
+	
     return (
         <div>
             <div>
@@ -10,6 +14,7 @@ const ProfileInfo = () => {
                     src='https://trawor.by/media/upload/image_20190726013709.jpg'/>
             </div>
             <div className={s.descriptionBlock}>
+			<img src={props.profile.photos.large} />
                 ava + description
             </div>
         </div>
