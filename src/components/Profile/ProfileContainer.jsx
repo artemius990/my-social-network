ac5a866cd0d540948fe91ec42d930602f2c0932c
Artem Lapenkov <artbreenche@gmail.com>
2021-05-16 12:35:51 +0200

    autorized UserId, stopSubmit

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -14,7 +14,8 @@ class ProfileContainer extends React.Component {
     componentDidMount() {
         let userId = this.props.match.params.userId;
         if (!userId) {
-            userId = 7553;
+            // userId = 7553;
+            userId = this.props.authorizedUserId;
         }        
         this.props.getUserProfile(userId)
         this.props.getStatus(userId);
@@ -23,7 +24,10 @@ class ProfileContainer extends React.Component {
 
     render() {    
         return (
-            <Profile {...this.props} profile={this.props.profile} status={this.props.status} updateStatus={this.props.updateStatus}/>
+            <Profile {...this.props} 
+            profile={this.props.profile} 
+            status={this.props.status} 
+            updateStatus={this.props.updateStatus}/>
         )
     }
 }
@@ -31,7 +35,9 @@ class ProfileContainer extends React.Component {
 
 let mapStateToProps = (state) => ({
     profile: state.profilePage.profile,
-    status: state.profilePage.status    
+    status: state.profilePage.status,
+    authorizedUserId: state.auth.userId,
+    isAuth: state.auth.isAuth  
 });
 
 export default compose(
